f11b2fc617d9d8f2f385e36332a0026f288616f3
megahomyak <g.megahomyak@gmail.com>
2024-08-09 07:50:28 +0500

    Debugging is hard

--- src/renderer.js
+++ src/renderer.js
@@ -3,8 +3,31 @@ let ctx = canvas.getContext("2d");
 
 let lines = ["BOLD", "AND", "BRASH"];
 
-function check(xi, yi) {
-    return true;
+function printN(n) {
+    return {
+        print: function() {
+            if (n != 0) {
+                console.log(...arguments);
+                n--;
+            }
+        }
+    };
+}
+
+let p = printN(50);
+
+function check(xi, yi) { // xi and yi are [0; 1)
+    let lineIndex = Math.floor(lines.length * yi);
+    let line = lines[lineIndex];
+    let charIndex = Math.floor(line.length * xi);
+    let char = line[charIndex];
+    let cx = xi / line.length; // "Character X"
+    let cy = yi / lines.length; // "Character Y"
+    p.print(cx, cy);
+    if (cx > 0.5 && cy > 0.5) {
+        return true;
+    }
+    return false;
 }
 
 function render() {
@@ -21,8 +44,8 @@ function render() {
 
     for (let x = 0; x < widthPx; ++x) {
         for (let y = 0; y < heightPx; ++y) {
-            let xi = (x + 1) / widthPx;
-            let yi = (y + 1) / heightPx;
+            let xi = x / widthPx;
+            let yi = y / heightPx;
             if (check(xi, yi)) {
                 ctx.fillRect(x, y, 1, 1);
             }
