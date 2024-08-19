22e50d71089402521cd9a95d9e8b2230bbed5d92
megahomyak <g.megahomyak@gmail.com>
2024-08-20 00:33:09 +0500

    Compressed some stuff. Will think about this tomorrow

--- src/renderer.js
+++ src/renderer.js
@@ -1,63 +1,60 @@
 let canvas = document.getElementsByTagName("canvas")[0];
 let ctx = canvas.getContext("2d");
 
-// any number in this function is in [0; 1)
-function check() {
-    // beginning's values shouldn't be greater than the corresponding values of end
-    function compressed({ beginning, end, renderer }) {
-        return ({ x, y }) => {
-            return (
-                x >= beginning.x && x < end.x
-                && y >= beginning.y && y < end.y
-                && renderer({
-                    x: (x - beginning.x) / (end.x - beginning.x),
-                    y: (y - beginning.y) / (end.y - beginning.y),
-                })
-            );
-        };
-    }
-
-    function circle() {
-        function distance(point1, point2) {
-            return Math.hypot(point2.x - point1.x, point2.y - point1.y);
-        }
-        return ({ x, y }) => {
-            return distance({ x, y }, { x: 0.5, y: 0.5 }) <= 0.5;
-        };
-    }
-
-    function letter({ characterCode }) {
-        return {
-
-        }[characterCode] || (({ x, y }) => {
-            // Rendering a question mark
-            return circle()({ x, y });
-        });
-    }
-
-    function partitionedHorizontally({ partAmount, renderer }) {
-        return ({ x, y }) => {
-            let partIndex = Math.floor(x * partAmount);
-            let partLength = 1 / partAmount;
-            let partBias = partLength * partIndex;
-            return renderer({ partIndex })({ x: (x - partBias) * partAmount, y });
-        };
-    }
-
-    function line({ content }) {
-        let characterGap = 0.2;
-        let rightmostGap = characterGap / content.length;
-        return compressed({
-            beginning: { x: 0, y: 0 }, end: { x: 1 - rightmostGap, y: 1 }, renderer: partitionedHorizontally({
-                partAmount: content.length, renderer: ({ partIndex }) => {
-                    return compressed({ beginning: { x: characterGap, y: 0 }, end: { x: 1, y: 1 }, renderer: letter({ characterCode: content[partIndex] }) });
-                }
-            })
-        });
-    }
-
-    return line({ content: "BOLD" });
+function debug() {
+    console.log(...arguments);
+    throw new Object();
 }
+
+// { x, y } are all in [0; 1)
+
+// beginning's values shouldn't be greater than the corresponding values of end
+let compressed = ({ beginning, end, renderer }) => ({ x, y }) => (
+    x >= beginning.x && x < end.x
+    && y >= beginning.y && y < end.y
+    && renderer({
+        x: (x - beginning.x) / (end.x - beginning.x),
+        y: (y - beginning.y) / (end.y - beginning.y),
+    })
+);
+
+let limitX = ({ beginning, end, renderer }) => ({ x, y }) => renderer({ x: (beginning + x * (1 - beginning)) * end, y });
+
+let circle = () => {
+    function distance(point1, point2) {
+        return Math.hypot(point2.x - point1.x, point2.y - point1.y);
+    }
+    return ({ x, y }) => distance({ x, y }, { x: 0.5, y: 0.5 }) <= 0.5;
+};
+
+let letter = ({ characterCode }) => ({
+
+}[characterCode] || (({ x, y }) => {
+    // Rendering a question mark
+    return circle()({ x, y });
+}));
+
+function partitionedHorizontally({ partAmount, partGap, renderer }) {
+    return ({ x, y }) => {
+        if (partAmount == 0) {
+            return false;
+        }
+        let partIndex = Math.floor(x * partAmount);
+        let compressedGap = partGap / partAmount;
+        return limitX({ beginning: compressedGap, end: 1, renderer: compressed({ beginning: { x: partGap, y: 0 }, end: { x: 1, y: 1 }, renderer: renderer({ partIndex }) }) })({ x, y });
+    };
+}
+
+function line({ gap, characters }) {
+    return partitionedHorizontally({ partAmount: characters.length, partGap: gap, renderer: ({ partIndex }) => circle() });
+    return partitionedHorizontally({
+        partAmount: characters.length, partGap: gap, renderer: ({ partIndex }) => {
+            return letter({ characterCode: characters[partIndex] });
+        }
+    });
+}
+
+let canvasImage = () => line({ characters: "BOLD", gap: 0.2 });
 
 function render() {
     let widthPx, heightPx;
@@ -81,7 +78,7 @@ function render() {
             for (let y = 0; y < heightPx; ++y) {
                 let xUnit = x / widthPx;
                 let yUnit = y / heightPx;
-                if (check()({ x: xUnit, y: yUnit })) {
+                if (canvasImage()({ x: xUnit, y: yUnit })) {
                     ctx.fillRect(x, y, 1, 1);
                 }
             }
