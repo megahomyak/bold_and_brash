35ecfcb469d8074f17e0b837d3b924cf22f1b178
megahomyak <g.megahomyak@gmail.com>
2024-08-19 23:01:42 +0500

    Rewriting. Made it way nicer and actually stretchy

--- src/renderer.js
+++ src/renderer.js
@@ -1,86 +1,65 @@
 let canvas = document.getElementsByTagName("canvas")[0];
 let ctx = canvas.getContext("2d");
 
-let lines = ["BOLD", "AND", "BRASH"];
+// any number in this function is in [0; 1)
+function check({x, y}) {
+    // "p1" is "point 1", the beginning point
+    // "p2" is "point 2", the ending point
+    // p1's values shouldn't be greater than the corresponding values of p2
+    function compress(p1, p2, renderer) {
+        return (
+            x >= p1.x && x < p2.x
+            && y >= p1.y && y < p2.y
+            && renderer({
+                x: (x - p1.x) / (p2.x - p1.x),
+                y: (y - p1.y) / (p2.y - p1.y),
+            })
+        );
+    }
 
-function printN(n) {
-    return {
-        print: function() {
-            if (n != 0) {
-                console.log(...arguments);
-                n--;
-            }
+    function circle() {
+        function distance(p1, p2) {
+            return Math.hypot(p2.x - p1.x, p2.y - p1.y);
         }
-    };
-}
-
-function check(xi, yi) { // xi and yi are [0; 1)
-    function getFractionalPart(f) {
-        return f % 1;
+        return ({x, y}) => {
+            return distance({x, y}, {x: 0.5, y: 0.5}) <= 0.5;
+        };
     }
 
-    function between(f, smallerBound, greaterBound) {
-        return f >= smallerBound && f <= greaterBound;
+    function line(string) {
+        return ({x, y}) => {
+            return 
+        };
     }
 
-    function getInner(container, measure) {
-        let containerMeasure = container.length * measure;
-        let itemIndex = Math.floor(containerMeasure);
-        let item = container[itemIndex];
-        let scaledMeasure = getFractionalPart(containerMeasure);
-        return [scaledMeasure, item];
-    }
-    let [cy, line] = getInner(lines, yi);
-    let [cx, char] = getInner(line, xi);
-
-    function rect(x1, y1, x2, y2) {
-        return between(cx, x1, x2) && between(cy, y1, y2);
-    }
-
-    function distance(x1, y1, x2, y2) {
-        let xl = Math.abs(x1 - x2);
-        let yl = Math.abs(y1 - y2);
-        let dist = Math.hypot(xl, yl);
-        return dist;
-    }
-
-    function circle(x, y, r) {
-        return distance(cx, cy, x, y) <= r;
-    }
-
-    function slopeUp(x1, y1, x2, y2) {
-        return (between(cx, x1, x2) && between(cy, y1, y2)) && ((x2 - cx) * (x2 - x1 / y2 - y1) <= cy - y1);
-    }
-
-    {
-        let dot = rect(0.4, 0.8, 0.6, 1.0);
-        let mainCircle = circle(0.5, 0.35, 0.35);
-        let circleCutout = circle(0.5, 0.35, 0.15);
-        let squareCutout = rect(0.0, 0.35, 0.5, 0.7);
-        let slope = slopeUp(0.0, 0.0, 1.0, 0.5);
-        return dot || (mainCircle && !circleCutout && !squareCutout) || slope;
-    }
-    return false;
+    return compress({x: 0, y: 0}, {x: 1, y: 1/3}, circle());
 }
 
 function render() {
-    let dpr = window.devicePixelRatio;
-    let widthPx = Math.floor(ctx.canvas.clientWidth * dpr);
-    let heightPx = Math.floor(ctx.canvas.clientHeight * dpr);
-    ctx.canvas.width = widthPx;
-    ctx.canvas.height = heightPx;
+    let widthPx, heightPx;
+    { // Making the canvas crisp with any DPR
+        let dpr = window.devicePixelRatio;
+        widthPx = Math.floor(ctx.canvas.clientWidth * dpr);
+        heightPx = Math.floor(ctx.canvas.clientHeight * dpr);
+        ctx.canvas.width = widthPx;
+        ctx.canvas.height = heightPx;
+    }
 
-    ctx.fillStyle = "#000000";
-    ctx.beginPath();
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#FFFFFF";
+    { // Filling the canvas with black
+        ctx.fillStyle = "#000000";
+        ctx.beginPath();
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "#FFFFFF";
+    }
 
-    for (let x = 0; x < widthPx; ++x) {
-        for (let y = 0; y < heightPx; ++y) {
-            let xi = x / widthPx;
-            let yi = y / heightPx;
-            if (check(xi, yi)) {
-                ctx.fillRect(x, y, 1, 1);
+    { // Drawing every pixel
+        for (let x = 0; x < widthPx; ++x) {
+            for (let y = 0; y < heightPx; ++y) {
+                let xi = x / widthPx;
+                let yi = y / heightPx;
+                if (check({x: xi, y: yi})) {
+                    ctx.fillRect(x, y, 1, 1);
+                }
             }
         }
     }
