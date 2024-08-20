cfdd7e756af058ac1ec5e7851a980cad28392a95
megahomyak <g.megahomyak@gmail.com>
2024-08-20 23:46:01 +0500

    Added: "O", "L". In progress: "A"

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,7 @@
-let canvas = document.getElementsByTagName("canvas")[0];
-let ctx = canvas.getContext("2d");
+const canvas = document.getElementsByTagName("canvas")[0];
+const ctx = canvas.getContext("2d");
 
-let debug = {
+const debug = {
     print() {
         console.log(...arguments);
         throw "debug throw";
@@ -31,11 +31,11 @@ function distance(point1, point2) {
     return Math.hypot(point2.x - point1.x, point2.y - point1.y);
 }
 
-let combined = (...renderers) => ({ x, y }) => renderers.map(renderer => renderer({ x, y })).some(result => result);
-let carved = ({ renderer, carver }) => ({ x, y }) => renderer({ x, y }) && !carver({ x, y });
+const combined = (...renderers) => coordinates => renderers.map(renderer => renderer(coordinates)).some(result => result);
+const carved = ({ renderer, carver }) => coordinates => renderer(coordinates) && !carver(coordinates);
 // Beginning and end are { x: [0; 1], y: [0; 1] }
 // Input requirements: beginning.x < end.x; beginning.y < end.y
-let positioned = ({ beginning, end, renderer }) => ({ x, y }) => (
+const positioned = ({ beginning, end, renderer }) => ({ x, y }) => (
     x >= beginning.x && x < end.x
     && y >= beginning.y && y < end.y
     && renderer({
@@ -43,9 +43,10 @@ let positioned = ({ beginning, end, renderer }) => ({ x, y }) => (
         y: (y - beginning.y) / (end.y - beginning.y),
     })
 );
-let filled = () => ({ x: _x, y: _y }) => true;
-let circle = () => ({ x, y }) => distance({ x, y }, { x: 0.5, y: 0.5 }) <= 0.5;
-let questionMark = () => combined(
+const filled = () => ({ x: _x, y: _y }) => true;
+const reversedByX = renderer => ({ x, y }) => renderer({ x: 1 - x, y });
+const circle = () => ({ x, y }) => distance({ x, y }, { x: 0.5, y: 0.5 }) <= 0.5;
+const questionMark = () => combined(
     // Lower dot
     positioned({ beginning: { x: 0.4, y: 0.825 }, end: { x: 0.6, y: 1 }, renderer: filled() }),
     // Upper part circle
@@ -61,18 +62,32 @@ let questionMark = () => combined(
     // Circle down stem
     positioned({ beginning: { x: 0.4, y: 0.45 }, end: { x: 0.6, y: 0.7 }, renderer: filled() }),
 );
-let letter = ({ characterCode }) => ({
-
+const slope = () => ({ x, y }) => x < y;
+const letter = ({ characterCode }) => ({
+    "L": combined(
+        positioned({ beginning: { x: 0, y: 0 }, end: { x: 1 / 3, y: 1 }, renderer: filled() }),
+        positioned({ beginning: { x: 0, y: 2 / 3 }, end: { x: 1, y: 1 }, renderer: filled() }),
+    ),
+    "O": carved({
+        renderer: circle(),
+        carver: positioned({ beginning: { x: 0.25, y: 0.25 }, end: { x: 0.75, y: 0.75 }, renderer: circle() })
+    }),
+    "A": combined(
+        carved({
+            renderer: reversedByX(slope()),
+            carver: positioned({ beginning: { x: 0.2, y: 0.2 }, end: { x: 1, y: 1 }, renderer: reversedByX(slope()) })
+        })
+    ),
 }[characterCode] || questionMark());
-let partitioned = ({ coordinate, partAmount, partGap, renderer }) => {
+const partitioned = ({ coordinate, partAmount, partGap, renderer }) => {
     if (partAmount == 0) { return false; }
-    let leftmostGap = partGap / partAmount;
+    const leftmostGap = partGap / partAmount;
     // Shifting the coordinate to after the leftmost gap before doing any processing
     coordinate = leftmostGap + coordinate * (1 - leftmostGap);
     let partIndex = Math.floor(coordinate * partAmount);
     if (partIndex == partAmount) partIndex--;
-    let partLength = 1 / partAmount;
-    let partBias = partIndex * partLength;
+    const partLength = 1 / partAmount;
+    const partBias = partIndex * partLength;
     // Getting the coordinate inside the part
     coordinate = (coordinate - partBias) * partAmount;
     if (coordinate < partGap) { return false; }
@@ -80,17 +95,16 @@ let partitioned = ({ coordinate, partAmount, partGap, renderer }) => {
     coordinate = (coordinate - partGap) / (1 - partGap);
     return renderer({ partIndex, coordinate });
 };
-let partitionedHorizontally = ({ partAmount, partGap, renderer }) => ({ x, y }) => partitioned({ coordinate: x, partAmount, partGap, renderer: ({ partIndex, coordinate }) => renderer({ partIndex })({ x: coordinate, y }) });
-let partitionedVertically = ({ partAmount, partGap, renderer }) => ({ x, y }) => partitioned({ coordinate: y, partAmount, partGap, renderer: ({ partIndex, coordinate }) => renderer({ partIndex })({ x, y: coordinate }) });
-let line = ({ line, characterGap }) => partitionedHorizontally({ partAmount: line.length, partGap: characterGap, renderer: ({ partIndex }) => letter({ characterCode: line[partIndex] }) });
-let lines = ({ lines, lineGap, characterGap }) => partitionedVertically({ partAmount: lines.length, partGap: lineGap, renderer: ({ partIndex }) => line({ line: lines[partIndex], characterGap }) });
-let canvasImage = lines({ lines: ["BOLD", "AND", "BRASH"], lineGap: 0.2, characterGap: 0.2 });
-//let canvasImage = circle();
+const partitionedHorizontally = ({ partAmount, partGap, renderer }) => ({ x, y }) => partitioned({ coordinate: x, partAmount, partGap, renderer: ({ partIndex, coordinate }) => renderer({ partIndex })({ x: coordinate, y }) });
+const partitionedVertically = ({ partAmount, partGap, renderer }) => ({ x, y }) => partitioned({ coordinate: y, partAmount, partGap, renderer: ({ partIndex, coordinate }) => renderer({ partIndex })({ x, y: coordinate }) });
+const line = ({ line, characterGap }) => partitionedHorizontally({ partAmount: line.length, partGap: characterGap, renderer: ({ partIndex }) => letter({ characterCode: line[partIndex] }) });
+const lines = ({ lines, lineGap, characterGap }) => partitionedVertically({ partAmount: lines.length, partGap: lineGap, renderer: ({ partIndex }) => line({ line: lines[partIndex], characterGap }) });
+const canvasImage = lines({ lines: ["BOLD", "AND", "BRASH"], lineGap: 0.2, characterGap: 0.2 });
 
 function render() {
     let widthPx, heightPx;
     { // Making the canvas crisp with any DPR
-        let dpr = window.devicePixelRatio;
+        const dpr = window.devicePixelRatio;
         widthPx = Math.floor(ctx.canvas.clientWidth * dpr);
         heightPx = Math.floor(ctx.canvas.clientHeight * dpr);
         ctx.canvas.width = widthPx;
@@ -107,8 +121,8 @@ function render() {
     { // Drawing every pixel
         for (let x = 0; x < widthPx; ++x) {
             for (let y = 0; y < heightPx; ++y) {
-                let xUnit = x * (1 / (widthPx - 1));
-                let yUnit = y * (1 / (heightPx - 1));
+                const xUnit = x * (1 / (widthPx - 1));
+                const yUnit = y * (1 / (heightPx - 1));
                 if (canvasImage({ x: xUnit, y: yUnit })) {
                     ctx.fillRect(x, y, 1, 1);
                 }
